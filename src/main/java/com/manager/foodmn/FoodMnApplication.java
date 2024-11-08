68798ba16f31061a62d1ef17e55fefa4cd51b524
joardam <pedrofdias@live.com>
2024-11-08 01:30:59 -0300

    recomeçando aplicação com configuração nova e funcional

---
+++ src/main/java/com/manager/foodmn/FoodMnApplication.java
@@ -0,0 +1,13 @@
+package com.manager.foodmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodMnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manager/foodmn/FoodMnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.foodmn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodMnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
