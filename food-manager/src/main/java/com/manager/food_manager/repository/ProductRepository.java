dbd6489567505695861d695e51817f1d582378eb
joardam <pedrofdias@live.com>
2024-11-08 01:25:04 -0300

    Um lindo recomeço

--- food-manager/src/main/java/com/manager/food_manager/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.manager.food_manager.repository;
-
-import com.manager.food_manager.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
