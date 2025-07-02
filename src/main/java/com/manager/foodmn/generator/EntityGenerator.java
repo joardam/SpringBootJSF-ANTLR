5585f85946a52ada73c48f6457dc38eaabc7c2eb
joardam <pedrofdias@live.com>
2025-07-02 16:07:24 -0300

    Construindo model , Repository e a ServiceInterface

---
+++ src/main/java/com/manager/foodmn/generator/EntityGenerator.java
@@ -0,0 +1,169 @@
+package com.manager.foodmn.generator;
+
+import com.manager.foodmn.antlr.EntityDefBaseVisitor;
+import com.manager.foodmn.antlr.EntityDefLexer;
+import com.manager.foodmn.antlr.EntityDefParser;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class EntityGenerator {
+
+    public static void main(String[] args) throws IOException {
+        String sourceDirectory = "src/main/resources/entities";
+        System.out.println("Procurando por arquivos .ent em: " + sourceDirectory);
+
+        try (Stream<Path> paths = Files.walk(Paths.get(sourceDirectory))) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .filter(path -> path.toString().endsWith(".ent"))
+                    .forEach(EntityGenerator::generateEntityFromFile);
+        }
+    }
+
+    private static void generateEntityFromFile(Path entityFile) {
+        try {
+            System.out.println("Processando arquivo: " + entityFile.getFileName());
+
+            CharStream cs = CharStreams.fromPath(entityFile);
+            EntityDefLexer lexer = new EntityDefLexer(cs);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            EntityDefParser parser = new EntityDefParser(tokens);
+            ParseTree tree = parser.entityFile();
+
+            EntityVisitor visitor = new EntityVisitor();
+            visitor.visit(tree);
+
+        } catch (IOException e) {
+            System.err.println("Erro ao processar o arquivo " + entityFile.getFileName());
+            e.printStackTrace();
+        }
+    }
+
+    static class EntityVisitor extends EntityDefBaseVisitor<Void> {
+        @Override
+        public Void visitEntityDef(EntityDefParser.EntityDefContext ctx) {
+            String className = ctx.ID().getText();
+            String entityFolderName = className.toLowerCase();
+
+            generateModel(className, entityFolderName, ctx.fieldDef());
+            generateRepository(className, entityFolderName);
+            generateServiceInterface(className, entityFolderName);
+            //generateServiceImplementation(className, entityFolderName);
+            //generateController(className, entityFolderName);
+            //generateView(className, entityFolderName, ctx.fieldDef());
+
+            return null;
+        }
+
+        private void generateModel(String className, String entityFolderName, List<EntityDefParser.FieldDefContext> fields) {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("package com.manager.foodmn.").append(entityFolderName).append(".model;\n\n");
+
+            sb.append("import jakarta.persistence.Entity;\n");
+            sb.append("import jakarta.persistence.Id;\n");
+            sb.append("import lombok.Data;\n");
+            sb.append("import java.math.BigDecimal;\n");
+            sb.append("import jakarta.persistence.GeneratedValue;\n");
+            sb.append("import jakarta.persistence.GenerationType;\n\n");
+
+            sb.append("@Data\n");
+            sb.append("@Entity\n");
+            sb.append("public class ").append(className).append(" {\n\n");
+
+            boolean isFirstField = true;
+            for (EntityDefParser.FieldDefContext fieldCtx : fields) {
+                if (isFirstField) {
+                    sb.append("    @Id\n");
+                    sb.append("    @GeneratedValue(strategy = GenerationType.SEQUENCE)\n");
+                    sb.append("    private Long id;\n\n");
+                    isFirstField = false;
+                }
+                String type = fieldCtx.type().getText();
+                String name = fieldCtx.ID().getText();
+                sb.append("    private ").append(type).append(" ").append(name).append(";\n\n");
+            }
+
+            sb.append("}\n");
+
+            Path outFile = Paths.get("src/main/java/com/manager/foodmn/" + entityFolderName + "/model/" + className + ".java");
+            try {
+                Files.createDirectories(outFile.getParent());
+                Files.writeString(outFile, sb.toString(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+                System.out.println("Classe de modelo gerada com sucesso: " + outFile);
+            } catch (IOException e) {
+                System.err.println("Erro ao gerar a classe de modelo para " + className);
+                e.printStackTrace();
+            }
+        }
+
+        private void generateRepository(String className, String entityFolderName) {
+            StringBuilder sb = new StringBuilder();
+            String repositoryClassName = className + "Repository";
+
+            sb.append("package com.manager.foodmn.").append(entityFolderName).append(".repository;\n\n");
+
+            sb.append("import com.manager.foodmn.").append(entityFolderName).append(".model.").append(className).append(";\n");
+            sb.append("import org.springframework.data.jpa.repository.JpaRepository;\n\n");
+
+            sb.append("public interface ").append(repositoryClassName).append(" extends JpaRepository<").append(className).append(", Long> {\n");
+            sb.append("\n}\n");
+
+            Path outFile = Paths.get("src/main/java/com/manager/foodmn/" + entityFolderName + "/repository/" + repositoryClassName + ".java");
+            try {
+                Files.createDirectories(outFile.getParent());
+                Files.writeString(outFile, sb.toString(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+                System.out.println("Interface de repositório gerada com sucesso: " + outFile);
+            } catch (IOException e) {
+                System.err.println("Erro ao gerar a interface de repositório para " + className);
+                e.printStackTrace();
+            }
+        }
+
+        private void generateServiceInterface(String className, String entityFolderName) {
+            StringBuilder sb = new StringBuilder();
+            String serviceInterfaceName = className + "Service";
+            String modelVariableName = className.substring(0, 1).toLowerCase() + className.substring(1);
+
+            sb.append("package com.manager.foodmn.").append(entityFolderName).append(".service;\n\n");
+
+            sb.append("import com.manager.foodmn.").append(entityFolderName).append(".model.").append(className).append(";\n");
+            sb.append("import java.util.List;\n");
+            sb.append("import java.util.Optional;\n\n");
+
+            sb.append("public interface ").append(serviceInterfaceName).append(" {\n\n");
+
+            sb.append("    ").append(className).append(" save(").append(className).append(" ").append(modelVariableName).append(");\n\n");
+            sb.append("    List<").append(className).append("> findAll();\n\n");
+            sb.append("    Optional<").append(className).append("> findById(Long id);\n\n");
+            sb.append("    ").append(className).append(" update(").append(className).append(" ").append(modelVariableName).append(");\n\n");
+            sb.append("    void deleteById(Long id);\n\n");
+            sb.append("    void deletebyObject(").append(className).append(" ").append(modelVariableName).append(");\n");
+
+            sb.append("}\n");
+
+            Path outFile = Paths.get("src/main/java/com/manager/foodmn/" + entityFolderName + "/service/" + serviceInterfaceName + ".java");
+            try {
+                Files.createDirectories(outFile.getParent());
+                Files.writeString(outFile, sb.toString(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+                System.out.println("Interface de serviço gerada com sucesso: " + outFile);
+            } catch (IOException e) {
+                System.err.println("Erro ao gerar a interface de serviço para " + className);
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+
+
+    }
+}
